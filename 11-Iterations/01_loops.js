6ca45de4475b126b751ae2fd57d301d99c13a221
ptk6 <ptkprateek6@gmail.com>
2025-06-29 22:35:59 +0530

    Adding Loops part

---
+++ 11-Iterations/01_loops.js
@@ -0,0 +1,24 @@
+//for
+
+//ctrl+d will select all instances of the word in the file
+for (let i = 0; i <= 10; i++) {
+  const element = i * 2; // Example operation: multiplying each index by 2
+  console.log(element);
+}
+
+//while
+
+let i = 0;
+while (i <= 10) {
+  const element = i * 2;
+  console.log(element);
+  i++;
+}
+
+// do while
+let score = 1;
+
+do {
+  console.log(`Score is ${score}`);
+  score++;
+} while (score <= 0);
